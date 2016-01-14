3a4ef35e67d94a6ab45cc609b461d5953a88f656
Robin van de Griend <robinvdgriend@outlook.com>
2016-01-14 09:10:33 +0100

    Add room validations

--- app/models/room.rb
+++ app/models/room.rb
@@ -2,4 +2,6 @@ class Room < ActiveRecord::Base
   has_many :memberships
   has_many :users, through: :memberships
   has_many :posts, dependent: :destroy
+
+  validates :name, presence: true, length: { in: 3..50 }
 end
--- test/models/room_test.rb
+++ test/models/room_test.rb
@@ -1,7 +1,26 @@
 require 'test_helper'
 
 class RoomTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @room = Room.new(name: "Example Room")
+  end
+
+  test "should be valid" do
+    assert @room.valid?
+  end
+
+  test "name should be non-blank" do
+    @room.name = ""
+    assert_not @room.valid?
+  end
+
+  test "name should have minimum length" do
+    @room.name = "a" * 2
+    assert_not @room.valid?
+  end
+
+  test "name should not exceed maximum length" do
+    @room.name = "a" * 51
+    assert_not @room.valid?
+  end
 end
