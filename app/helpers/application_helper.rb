d09d25e037b6fb3aa5947b9148d0ba35b1ddb9b4
Robin van de Griend <robinvdgriend@outlook.com>
2016-01-14 10:52:41 +0100

    Markup views for user

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def logged_in_user
     if user_id = session[:user_id]
-      return User.find_by(id: user_id)
+      return @current_user = User.find_by(id: user_id)
     else
       return false
     end
