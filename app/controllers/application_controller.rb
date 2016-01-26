b24a967043acc78482b2950b37fe708ce6100654
Robin van de Griend <robinvdgriend@outlook.com>
2016-01-26 15:39:22 +0100

    Move helper from module to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,14 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  include ApplicationHelper
+  # Make methods available to views
+  helper_method :logged_in_user
+
+  def logged_in_user
+    if user_id = session[:user_id]
+      return @current_user = User.find_by(id: user_id)
+    else
+      return false
+    end
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,2 @@
 module ApplicationHelper
-  def logged_in_user
-    if user_id = session[:user_id]
-      return @current_user = User.find_by(id: user_id)
-    else
-      return false
-    end
-  end
 end
