91c1d37b4df44f769f737134c4a850994b0a56e9
Robin van de Griend <robinvdgriend@outlook.com>
2016-01-26 20:33:38 +0100

    Add check for room role in user

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,4 +14,12 @@ class User < ActiveRecord::Base
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
     return BCrypt::Password.create(password, cost: cost)
   end
+
+  def is_room_member?(room)
+    room.memberships.where(user_id: self.id).any?
+  end
+
+  def is_room_moderator?(room)
+    self.is_room_member?(room) && room.memberships.find_by(user_id: self.id).role == "moderator"
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,8 +2,15 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   def setup
-    @user = users(:user_1)
-    @other_user = users(:user_2)
+    @user = User.first
+    @other_user = User.offset(1).first
+    @room = Room.first
+    
+    @room.users << @user
+  end
+
+  test "room should have member" do
+    assert @room.memberships.exists?(user_id: @user.id)
   end
 
   test "should be valid" do
@@ -40,5 +47,36 @@ class UserTest < ActiveSupport::TestCase
   test "email should not exceed maximum length" do
     @user.email = "a" * 256
     assert_not @user.valid?
+  end
+
+  test "is_room_member? should return true if user is a member of room" do
+    assert @user.is_room_member?(@room)
+  end
+
+  test "is_room_member? should return false if user is not a member of room" do
+    @room.users.destroy(@user)
+    assert_not @user.is_room_member?(@room)
+  end
+
+  test "is_room_member? should raise an error if room argument is invalid" do
+    assert_raises { @user.is_room_member?(String.new) }
+  end
+
+  test "is_room_moderator? should return true if user is a moderator of room" do
+    @room.memberships.find_by(user_id: @user.id).update(role: :moderator)
+    assert @user.is_room_moderator?(@room)
+  end
+
+  test "is_room_moderator? should return false if user is not a moderator of room" do
+    assert_not @user.is_room_moderator?(@room)
+  end
+
+  test "is_room_moderator? should return false if user is not in room" do
+    @room.users.destroy(@user)
+    assert_not @user.is_room_moderator?(@room)
+  end
+
+  test "is_room_moderator? should raise an error if room argument is invalid" do
+    assert_raises { @user.is_room_moderator?(String.new) }
   end
 end
