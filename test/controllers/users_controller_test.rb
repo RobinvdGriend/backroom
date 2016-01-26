24e35e0c88f3c742bbfedc466b7165f1285d7e50
Robin van de Griend <robinvdgriend@outlook.com>
2016-01-26 22:57:13 +0100

    Add test for user authorization

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,6 +1,11 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
+  def setup
+    @user = User.first
+    @other_user = User.offset(1).first
+  end
+
   test "should create user with valid signup information" do
     assert_difference "User.count", 1 do
       post :create, user: { name: Faker::Internet.user_name,
@@ -17,5 +22,22 @@ class UsersControllerTest < ActionController::TestCase
                             password: "invalid",
                             password_confirmation: "wrong" }
     end
+  end
+
+  test "should only show edit profile linked when user accesses their own page" do
+    get :show, id: @user.id
+    assert_select "a[href=?]", edit_user_path(@user), false
+
+    session[:user_id] = @user.id
+
+    get :show, id: @user.id
+    assert_select "a[href=?]", edit_user_path(@user)
+  end
+
+  test "should be redirected when trying to access the edit page of another user" do
+    session[:user_id] = @other_user.id
+
+    get :edit, id: @user.id
+    assert_redirected_to root_path
   end
 end
