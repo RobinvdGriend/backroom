2af3d795e6fa8f1c6a4da1403fc53c27ff2c0a1e
Robin van de Griend <robinvdgriend@outlook.com>
2016-01-26 15:52:11 +0100

    Move tests to represent changed method location

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,6 +1,21 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
+  def setup
+    @user = users(:user_1)
+  end
+
+  test "logged_in_user should return logged in user if logged in" do
+    # Log in user by setting the session cookie
+    session[:user_id] = @user.id
+    assert logged_in_user == @user
+  end
+
+  test "logged_in_user should return false when not logged in" do
+    session[:user_id] = nil
+    assert_not logged_in_user
+  end
+
   test "should create user with valid signup information" do
     assert_difference "User.count", 1 do
       post :create, user: { name: Faker::Internet.user_name,
